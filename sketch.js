ec79ced1afae8d9006c0be6c2da1b3cb4f90b2ee
geisieli <00001113387932sp@al.educacao.sp.gov.br>
2024-06-11 20:43:02 -0300

    adicionando arquivo js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+    stroke("yellow");
+    fill("blue");
+    
+    if(mouseIsPressed){
+      rect(mouseX, mouseY, 25, 40);
+    }
+  }
+  
